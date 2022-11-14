c3c894b14162f706413ffe4a9aa4ff75917e6452
sogud <github.sogud@outlook.com>
2022-11-14 15:02:09 +0800

    fix: load config

--- src/initConfig.ts
+++ src/initConfig.ts
@@ -1,42 +1,42 @@
-import fs from "fs"
-import path from "path"
-import log from "@/log"
-import loadConfigFile from "@/loadConfigFile"
-import { deepAssign } from "@/utils"
-import { defaultUserConfig } from "./languages.config"
-export const basePath = process.cwd()
-export const pkgBasePath = path.resolve(__dirname, "../")
-export const pkg = require(`${pkgBasePath}/package.json`)
+import fs from 'fs';
+import path from 'path';
+import log from '@/log';
+import { deepAssign } from '@/utils';
+import { defaultUserConfig } from './languages.config';
+export const basePath = process.cwd();
+export const pkgBasePath = path.resolve(__dirname, '../');
+export const pkg = require(`${pkgBasePath}/package.json`);
 
 const sysConfig = {
   basePath,
   pkgBasePath,
   pkg
-}
+};
+const userConfigPath = path.join(basePath, 'languages.config.js');
 
-const getUserConfig = async (testConfig: Partial<NodeJS.Global["config"]> = {}) => {
-  const userConfigPath = path.join(basePath, "languages.config.js")
-
+const getUserConfig = async () => {
   if (!fs.existsSync(userConfigPath)) {
-    return defaultUserConfig
+    return defaultUserConfig;
   }
-
-  let userConfig = {}
+  let userConfig = {};
   try {
-    const userConfigs = await loadConfigFile(userConfigPath)
-    userConfig = userConfigs?.[0] || {}
+    const { default: languagesConfig } = await import(`${userConfigPath}`);
+    userConfig = languagesConfig;
   } catch (error: any) {
     log.error({
       title: `${error.name}：Failed to load the configuration file.`,
       path: userConfigPath,
       detail: error.message
-    })
+    });
   }
-  const combineConfig = deepAssign<NodeJS.Global["config"]>(defaultUserConfig, userConfig)
-  return combineConfig
-}
+  const combineConfig = deepAssign<NodeJS.Global['config']>(
+    defaultUserConfig,
+    userConfig
+  );
+  return combineConfig;
+};
 
 export default async () => {
-  global.config = Object.assign(sysConfig, await getUserConfig())
-  global.log = log
-}
+  global.config = Object.assign(sysConfig, await getUserConfig());
+  global.log = log;
+};
