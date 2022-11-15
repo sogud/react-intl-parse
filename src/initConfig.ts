1b4910ca5b8aeea359b7f26defe2454a35d43f9d
sogud <github.sogud@outlook.com>
2022-11-15 15:43:37 +0800

    fix: loadConfigFile

--- src/initConfig.ts
+++ src/initConfig.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import log from '@/log';
 import { deepAssign } from '@/utils';
 import { defaultUserConfig } from './languages.config';
+import loadConfigFile from '@/loadConfigFile';
 export const basePath = process.cwd();
 export const pkgBasePath = path.resolve(__dirname, '../');
 export const pkg = require(`${pkgBasePath}/package.json`);
@@ -20,8 +21,9 @@ const getUserConfig = async () => {
   }
   let userConfig = {};
   try {
-    const { default: languagesConfig } = await import(`${userConfigPath}`);
-    userConfig = languagesConfig;
+    // const { default: sourceFile } = await import(`${userConfigPath}`);
+    const [sourceFile = {}] = await loadConfigFile(userConfigPath);
+    userConfig = sourceFile;
   } catch (error: any) {
     log.error({
       title: `${error.name}：Failed to load the configuration file.`,
